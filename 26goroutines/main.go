131398cc7fad90e09e5f29655ea9f35a14c34465
ManthanK-24 <manthandec@gmail.com>
2024-08-27 08:56:28 +0530

    waitGroup Project Added

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup // these are used as pointers
+
 func main() {
-   go greeter("Hello")
-   greeter("World")
+	//    go greeter("Hello")
+	//    greeter("World")
 
-}
+	websiteList := []string{
+		"https://google.com",
+		"https://instagram.com",
+		"https://github.com",
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(10*time.Millisecond)
-		fmt.Println(s)
 	}
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+    wg.Wait()
 }
+
+
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	
+	resp, err := http.Get(endpoint)
+	if err!=nil{
+		fmt.Println("Oops in endpoint")
+	}
+	fmt.Printf("%d Status Code for %s\n", resp.StatusCode, endpoint)
+}
+
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(10*time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
