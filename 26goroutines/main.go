c156353a36fa7ba0c20925da4fb16e20b1ca94ba
ManthanK-24 <manthandec@gmail.com>
2024-08-27 08:24:55 +0530

    Go Routines Project Added

---
+++ 26goroutines/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+   go greeter("Hello")
+   greeter("World")
+
+}
+
+func greeter(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(10*time.Millisecond)
+		fmt.Println(s)
+	}
+}
